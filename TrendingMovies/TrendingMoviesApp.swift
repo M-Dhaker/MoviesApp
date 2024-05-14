d7c7620bb2669d632d45bffa3caf98ec397116e1
TRIMECH-Dhaker <d.trimech@elsevier.com>
2024-05-14 11:33:06 +0200

    Initial project setup with SwiftUI and Combine

---
+++ TrendingMovies/TrendingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMoviesApp.swift
+//  TrendingMovies
+//
+//  Created by Dhaker Trimech on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
